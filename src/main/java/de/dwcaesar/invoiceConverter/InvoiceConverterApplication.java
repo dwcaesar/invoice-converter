ec8e44f44219cff4351c3e32f1d602596a976467
Daniel Wohlers <30534170+dwcaesar@users.noreply.github.com>
2025-01-31 08:36:51 +0100

    Init: Empty Spring Boot App
    Signed-off-by: Wohlers, Daniel <daniel.wohlers@zuehlke.com>

---
+++ src/main/java/de/dwcaesar/invoiceConverter/InvoiceConverterApplication.java
@@ -0,0 +1,13 @@
+package de.dwcaesar.invoiceConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dwcaesar/invoiceConverter/InvoiceConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dwcaesar.invoiceConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
